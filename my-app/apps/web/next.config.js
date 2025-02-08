a904aba3e0667a8ebb26316cbde293b8d30d7758
Eli-Gooding <eli.w.gooding@gmail.com>
2025-02-08 16:31:32 -0600

    fix: simplify app structure

--- my-app/apps/web/next.config.js
+++ my-app/apps/web/next.config.js
@@ -1,9 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@workspace/ui"],
-  experimental: {
-    // This will allow Next.js to resolve packages from the monorepo
-    outputFileTracingRoot: "../../",
-    serverActions: true,
-  }
-}
+module.exports = {}
